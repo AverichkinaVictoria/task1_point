9a79dd0b26213800f8a58c31eb931d7345c8f964
AverichkinaVictoria <averichkina2000@mail.ru>
2019-09-26 02:07:28 +0300

    final version 3.0

--- src/xipoint.cpp
+++ src/xipoint.cpp
@@ -4,23 +4,192 @@
 // Version:      0.2.1
 // Date:         08.09.2017
 //
-// This is a part of the course "Algorithms and Data Structures" 
-// provided by  the School of Software Engineering of the Faculty 
+// This is a part of the course "Algorithms and Data Structures"
+// provided by  the School of Software Engineering of the Faculty
 // of Computer Science at the Higher School of Economics.
 ////////////////////////////////////////////////////////////////////////////////
 
-
+////////////////////////////////////////////////////////////////////////////////
+// Student:      Averichkina Victoria
+// Group:        184-1
+// Date:         19.09.2019
+////////////////////////////////////////////////////////////////////////////////
 #include "xipoint.h"
 
 #include <algorithm>        // min
 #include <cmath>
 
-namespace xi {
+namespace xi
+{
+    Point::Point(int x, int y)
+    {
+        _x = x;
+        _y = y;
+    }
+    void Point::shift(int shf)
+    {
+        _x += shf;
+        _y += shf;
+    }
 
+    void Point::shift(int xSh, int ySh)
+    {
+        _x += xSh;
+        _y += ySh;
+    }
 
+    void Point::shift(const Point &pt)
+    {
+        _x += pt._x;
+        _y += pt._y;
+    }
 
-// TODO: put the method implementations here
+    double Point::dist(const Point &other) const
+    {
+        return (sqrt(pow(_x-other._x,2)+pow(_y-other._y,2)));
+    }
 
+    int Point::getX() const
+    {
+        return _x;
+    }
+
+    int Point::getY() const
+    {
+        return _y;
+    }
+
+    void Point::setX(int x)
+    {
+        _x = x;
+    }
+
+    void Point::setY(int y)
+    {
+        _y = y;
+    }
+
+    PointArray::PointArray()
+    {
+        _size = 0;
+        _arr = nullptr;
+    }
+
+    PointArray::PointArray(const Point points[], PointArray::Uint n)
+    {
+        _size = n;
+        _arr = new Point[_size];
+        for (int i = 0; i < _size; ++i)
+        {
+            _arr[i] = points[i];
+        }
+    }
+
+    PointArray::PointArray(const PointArray &pv)
+    {
+        _size = pv.getSize();
+        _arr = new Point[_size];
+        const Point* extra_copied = pv.getRawPtr();       //copy
+        for (int i = 0; i < _size; ++i)
+        {
+            _arr[i] = extra_copied[i];
+        }
+    }
+
+    PointArray::~PointArray()
+    {
+        clear();
+    }
+
+    void PointArray::resize(PointArray::Uint n)
+    {
+        Point* extra = new Point[n];
+        int new_size = std::min(n,_size);
+        _size = n;
+        for (int i = 0; i < new_size; ++i)
+        {
+            extra[i] = _arr[i];
+        }
+        delete[] _arr;
+        _arr = extra;
+    }
+
+    PointArray::Uint PointArray::getSize() const
+    {
+        return _size;
+    }
+
+    void PointArray::append(const Point &pt)
+    {
+        resize(_size+1);
+        _arr[_size-1] = pt;
+    }
+
+    void PointArray::insert(const Point &pt, PointArray::Uint pos)
+    {
+        resize(_size+1);
+        if(pos < _size-1)     //as we use uint there is no need to predict pos<0
+        {
+            for (int i = _size-1; i > pos; --i)
+            {
+                _arr[i] = _arr[i-1];
+            }
+            _arr[pos] = pt;
+        }
+        else
+        {
+            resize(_size-1);
+            append(pt);
+        }
+    }
+
+    void PointArray::remove(PointArray::Uint pos)
+    {
+        if(pos < _size)
+        {
+            for (int i = pos; i < _size-1; ++i)     //the part before pos remains unchanged
+            {
+                _arr[i] = _arr[i+1];
+            }
+            resize(_size-1);
+        }
+    }
+
+    void PointArray::clear()
+    {
+        if(_arr != nullptr)
+        {
+            delete[] _arr;
+            _arr = nullptr;
+        }
+        _size = 0;
+    }
+
+    double PointArray::computePath() const
+    {
+        double sum = 0;
+        for (int i = 0; i < _size-1; ++i)
+        {
+            sum += _arr[i].dist(_arr[i+1]);
+        }
+        return sum;
+    }
+
+    Point *PointArray::get(PointArray::Uint pos)
+    {
+        if(pos >= _size)
+            return nullptr;
+        else
+            return _arr+pos;
+    }
+
+    const Point *PointArray::get(PointArray::Uint pos) const
+    {
+        if(pos >= _size)
+            return nullptr;
+        else
+            return _arr+pos;
+    }
 
 
 } // namespace xi {
